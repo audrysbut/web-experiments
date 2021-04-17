627296c93cd8cae5dd4524a369e3e180749ea516
Audrius Butkus <audrius.butkus@telesoftas.com>
2021-04-17 17:18:47 +0300

    fix: fixed state managment in sudoku page

--- src/pages/sudoku-solver/SudokuPage.tsx
+++ src/pages/sudoku-solver/SudokuPage.tsx
@@ -2,6 +2,18 @@ import { useState } from "react";
 import { Sudoku } from "./sudoku/Sudoku";
 
 export const SudokuPage = () => {
-  const [state] = useState(new Array<number | undefined>(81));
-  return <Sudoku input={state} viewOnly={false} />;
+  const [state, setState] = useState(new Array<number | undefined>(81));
+  return (
+    <Sudoku
+      viewOnly={false}
+      input={state}
+      onValueChanged={(index, val) => {
+        setState((prev) => {
+          const newState = [...prev];
+          newState[index] = val;
+          return newState;
+        });
+      }}
+    />
+  );
 };
