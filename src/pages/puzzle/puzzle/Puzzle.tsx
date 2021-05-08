501c9190d6ea3286c6f59b5668dcd5088d711d8c
Audrius Butkus <audrius.butkus@telesoftas.com>
2021-05-09 01:22:52 +0300

    chore: commented code

--- src/pages/puzzle/puzzle/Puzzle.tsx
+++ src/pages/puzzle/puzzle/Puzzle.tsx
@@ -16,25 +16,25 @@ interface PuzzleProps {
   imageUrl: string;
 }
 
-function shuffle<T>(array: T[]): T[] {
-  let currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
+// function shuffle<T>(array: T[]): T[] {
+//   let currentIndex = array.length,
+//     temporaryValue,
+//     randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+//   // While there remain elements to shuffle...
+//   while (0 !== currentIndex) {
+//     // Pick a remaining element...
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+//     // And swap it with the current element.
+//     temporaryValue = array[currentIndex];
+//     array[currentIndex] = array[randomIndex];
+//     array[randomIndex] = temporaryValue;
+//   }
 
-  return array;
-}
+//   return array;
+// }
 
 const getPosition = (
   part: number
