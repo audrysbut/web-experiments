499c479d812a67671ea65027d26178c416b8aef9
Audrius <audrius.butkus@barbora.lt>
2022-08-27 08:28:51 +0300

    fix: fix broken styling

--- src/pages/sport/SportPage.tsx
+++ src/pages/sport/SportPage.tsx
@@ -16,75 +16,75 @@ const NavigationComponent = ({ second }: NavigationComponentProps) => {
 
 export const SportPage = () => {
   return (
-    <table>
+    <table className="tableSport">
       <thead>
-        <th>Exercise</th>
-        <th>Reps</th>
-        <th>Count</th>
-        <th>Laikas</th>
+        <th className="sport">Exercise</th>
+        <th className="sport">Reps</th>
+        <th className="sport">Count</th>
+        <th className="sport">Laikas</th>
       </thead>
       <tbody>
         <tr>
-          <td>13</td>
-          <td>3</td>
-          <td>15</td>
-          <td>
+          <td className="sport">13</td>
+          <td className="sport">3</td>
+          <td className="sport">15</td>
+          <td className="sport">
             <NavigationComponent second={6} />
           </td>
         </tr>
-        <tr>
-          <td>12</td>
-          <td>3</td>
-          <td>12</td>
-          <td>
+        <tr className="sport">
+          <td className="sport">12</td>
+          <td className="sport">3</td>
+          <td className="sport">12</td>
+          <td className="sport">
             <NavigationComponent second={24} />
           </td>
         </tr>
         <tr>
-          <td>2</td>
-          <td>3</td>
-          <td>10</td>
-          <td>
+          <td className="sport">2</td>
+          <td className="sport">3</td>
+          <td className="sport">10</td>
+          <td className="sport">
             <NavigationComponent second={45} />
           </td>
         </tr>
         <tr>
-          <td>4</td>
-          <td>3</td>
-          <td>10</td>
-          <td>
+          <td className="sport">4</td>
+          <td className="sport">3</td>
+          <td className="sport">10</td>
+          <td className="sport">
             <NavigationComponent second={64} />
           </td>
         </tr>
         <tr>
-          <td>6</td>
-          <td>3</td>
-          <td>12</td>
-          <td>
+          <td className="sport">6</td>
+          <td className="sport">3</td>
+          <td className="sport">12</td>
+          <td className="sport">
             <NavigationComponent second={87} />
           </td>
         </tr>
         <tr>
-          <td>14</td>
-          <td>3</td>
-          <td>12</td>
-          <td>
+          <td className="sport">14</td>
+          <td className="sport">3</td>
+          <td className="sport">12</td>
+          <td className="sport">
             <NavigationComponent second={116} />
           </td>
         </tr>
         <tr>
-          <td>7</td>
-          <td>3</td>
-          <td>10</td>
-          <td>
+          <td className="sport">7</td>
+          <td className="sport">3</td>
+          <td className="sport">10</td>
+          <td className="sport">
             <NavigationComponent second={145} />
           </td>
         </tr>
         <tr>
-          <td>8</td>
-          <td>3</td>
-          <td>10</td>
-          <td>
+          <td className="sport">8</td>
+          <td className="sport">3</td>
+          <td className="sport">10</td>
+          <td className="sport">
             <NavigationComponent second={162} />
           </td>
         </tr>
