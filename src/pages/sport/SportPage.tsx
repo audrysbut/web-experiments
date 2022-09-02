4807243b5ecd769c877bd3edb7aa8ab27e0ac048
Audrius <audrius.butkus@barbora.lt>
2022-09-02 13:43:33 +0300

    feat: added second sport day data

--- src/pages/sport/SportPage.tsx
+++ src/pages/sport/SportPage.tsx
@@ -44,6 +44,49 @@ const day1: SportData[] = [
   },
 ];
 
+const day2: SportData[] = [
+  {
+    muscleGroup: "Apšilimas",
+    exercise: "Dviratis",
+    series: ["5 min"],
+  },
+  {
+    muscleGroup: "Presas",
+    exercise: "Susirietimai įstrižiniams",
+    series: ["4x12"],
+  },
+  {
+    muscleGroup: "Kojos",
+    exercise: "Lenkimas sėdint",
+    series: ["4x15 25 Kg"],
+  },
+  {
+    muscleGroup: "Kojos",
+    exercise: "Lenkimas gulint",
+    series: ["3x12 10 Kg"],
+  },
+  {
+    muscleGroup: "Kojos",
+    exercise: "Tiesimas sėdint paskui pamažinant svorį",
+    series: ["4x12+12 2x10 Kg"],
+  },
+  {
+    muscleGroup: "Kojos",
+    exercise: "Kojų platforma gulint",
+    series: ["4x15 50 Kg"],
+  },
+  {
+    muscleGroup: "Kojos",
+    exercise: "Suvedimai",
+    series: ["4x15 35 Kg"],
+  },
+  {
+    muscleGroup: "Blauzdos",
+    exercise: "Su koju platforma didinant kas plytele",
+    series: ["18x 40 Kg", "15x50 Kg", "12x60 Kg", "10x70 Kg", "08x80 Kg"],
+  },
+];
+
 interface ExerciseTableProps {
   data: SportData[];
 }
@@ -81,6 +124,9 @@ export const SportPage = () => {
     >
       <span>1 diena</span>
       <ExerciseTable data={day1} />
+      <p />
+      <span>2 diena</span>
+      <ExerciseTable data={day2} />
     </div>
   );
 };
