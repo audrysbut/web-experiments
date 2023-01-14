abdbfad6eedb5c81f8dcb300f7e9388bab20979b
Audrius <audrysbut@gmail.com>
2023-01-14 21:31:06 +0200

    chore: router change

--- apps/web-experiments/src/app/app.tsx
+++ apps/web-experiments/src/app/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { SudokuPage } from "./pages/sudoku-solver/SudokuPage";
 import { NavigationBar } from "./NavigationBar";
 import { RubikPage } from "./pages/rubik/RubikPage";
