2897ef43f8a337ed5c7560eb0532c91037c4201c
Audrius <audrysbut@gmail.com>
2023-07-06 13:38:27 +0300

    chore: temp

--- apps/web-experiments/src/app/pages/graph/graph-page.tsx
+++ apps/web-experiments/src/app/pages/graph/graph-page.tsx
@@ -7,28 +7,86 @@ const mbti: Node<TitleData> = {
     {
       content: { title: 'Ni' },
       nodes: [
-        { id: '111', content: { title: 'Te' } },
-        { id: '112', content: { title: 'Fe' } },
+        {
+          id: '111',
+          content: { title: 'Te' },
+          nodes: [
+            {
+              id: '1111',
+              content: { title: 'Fi' },
+              nodes: [{ id: '11111', content: { title: 'Se' } }],
+            },
+          ],
+        },
+        {
+          id: '112',
+          content: { title: 'Fe' },
+          nodes: [
+            {
+              id: '1121',
+              content: { title: 'Ti' },
+              nodes: [{ id: '11211', content: { title: 'Se' } }],
+            },
+          ],
+        },
       ],
     },
     {
       content: { title: 'Te' },
       nodes: [
-        { id: '121', content: { title: 'Si' } },
-        { id: '121', content: { title: 'Ni' } },
+        {
+          id: '121',
+          content: { title: 'Si' },
+          nodes: [
+            {
+              id: '1211',
+              content: { title: 'Ne' },
+              nodes: [{ id: '12111', content: { title: 'Fi' } }],
+            },
+          ],
+        },
+        {
+          id: '122',
+          content: { title: 'Ni' },
+          nodes: [
+            {
+              id: '1221',
+              content: { title: 'Se' },
+              nodes: [{ id: '12211', content: { title: 'Fi' } }],
+            },
+          ],
+        },
       ],
     },
     {
       content: { title: 'Fi' },
       nodes: [
-        { id: '131', content: { title: 'Ne' } },
-        { id: '132', content: { title: 'Se' } },
+        {
+          id: '131',
+          content: { title: 'Ne' },
+          nodes: [
+            { content: { title: 'Si' }, nodes: [{ content: { title: 'Te' } }] },
+          ],
+        },
+        {
+          id: '132',
+          content: { title: 'Se' },
+          nodes: [
+            { content: { title: 'Ni' }, nodes: [{ content: { title: 'Te' } }] },
+          ],
+        },
       ],
     },
     {
       content: { title: 'Se' },
       nodes: [
-        { id: '141', content: { title: 'Ti' } },
+        {
+          id: '141',
+          content: { title: 'Ti' },
+          nodes: [
+            { content: { title: 'Fe' }, nodes: [{ content: { title: 'Ni' } }] },
+          ],
+        },
         { id: '142', content: { title: 'Fi' } },
       ],
     },
@@ -63,66 +121,66 @@ const mbti: Node<TitleData> = {
   ],
 };
 
-const graph: Node<TitleData> = {
-  content: { title: '1 ' },
-  nodes: [
-    {
-      content: { title: '11' },
-      nodes: [
-        {
-          content: { title: '111' },
-          nodes: [
-            {
-              content: { title: '1111' },
-            },
-            {
-              content: { title: '1112' },
-            },
-            {
-              content: { title: '1113' },
-            },
-          ],
-        },
-        {
-          content: { title: '112' },
-          nodes: [
-            {
-              id: '1121',
-              content: { title: '1121' },
-            },
-          ],
-        },
-        {
-          content: { title: '113' },
-        },
-        {
-          content: { title: '114' },
-        },
-        {
-          content: { title: '115' },
-          nodes: [
-            { id: '1151', content: { title: '1151' } },
-            { id: '1152', content: { title: '1152' } },
-          ],
-        },
-        {
-          content: { title: '116' },
-        },
-      ],
-    },
-    {
-      content: { title: '12' },
-      nodes: [
-        {
-          content: { title: '121' },
-        },
-        {
-          content: { title: '122' },
-        },
-      ],
-    },
-  ],
-};
+// const graph: Node<TitleData> = {
+//   content: { title: '1 ' },
+//   nodes: [
+//     {
+//       content: { title: '11' },
+//       nodes: [
+//         {
+//           content: { title: '111' },
+//           nodes: [
+//             {
+//               content: { title: '1111' },
+//             },
+//             {
+//               content: { title: '1112' },
+//             },
+//             {
+//               content: { title: '1113' },
+//             },
+//           ],
+//         },
+//         {
+//           content: { title: '112' },
+//           nodes: [
+//             {
+//               id: '1121',
+//               content: { title: '1121' },
+//             },
+//           ],
+//         },
+//         {
+//           content: { title: '113' },
+//         },
+//         {
+//           content: { title: '114' },
+//         },
+//         {
+//           content: { title: '115' },
+//           nodes: [
+//             { id: '1151', content: { title: '1151' } },
+//             { id: '1152', content: { title: '1152' } },
+//           ],
+//         },
+//         {
+//           content: { title: '116' },
+//         },
+//       ],
+//     },
+//     {
+//       content: { title: '12' },
+//       nodes: [
+//         {
+//           content: { title: '121' },
+//         },
+//         {
+//           content: { title: '122' },
+//         },
+//       ],
+//     },
+//   ],
+// };
 
 export const GraphPage = () => {
   const params: NodeParams = {
@@ -134,7 +192,7 @@ export const GraphPage = () => {
   return (
     <>
       <GraphView graph={mbti} params={params} />
-      <GraphView graph={graph} params={params} />
+      {/* <GraphView graph={graph} params={params} /> */}
     </>
   );
 };
