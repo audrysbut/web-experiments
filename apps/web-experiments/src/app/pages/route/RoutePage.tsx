3b211ea7a36326e0262f3eb937956915e25a671f
Audrius <audrysbut@gmail.com>
2023-07-18 22:23:17 +0300

    chore: fix routing

--- apps/web-experiments/src/app/pages/route/RoutePage.tsx
+++ apps/web-experiments/src/app/pages/route/RoutePage.tsx
@@ -1,11 +1,10 @@
 import {
   Board,
-  EmptySpaceMapObject,
+  BoardSettings,
   MapObject,
   NavigationMapObject,
   Scaffolding,
 } from '@web-projects/map-routing';
-import { BoardSettings } from 'libs/map-routing/src/lib/map-items/board-context';
 
 function loadMapObjects(): MapObject[] {
   const mapItems: MapObject[] = [];
@@ -20,34 +19,10 @@ function loadMapObjects(): MapObject[] {
   return mapItems;
 }
 
-// function loadSpace(
-//   obsticles: MapObject[],
-//   settings: BoardSettings
-// ): MapObject[] {
-//   const spaces: MapObject[] = [];
-//   for (let x = 0; x < settings.width; x += settings.step) {
-//     for (let y = 0; y < settings.height; y += settings.step) {
-//       const isObsticle = obsticles.some((o) => o.isCollision(x, y));
-//       if (!isObsticle) {
-//         const emptySpace = new EmptySpaceMapObject(
-//           x,
-//           y,
-//           settings.step,
-//           settings.step
-//         );
-//         spaces.push(emptySpace);
-//       }
-//     }
-//   }
-//   return spaces;
-// }
-
 const settings: BoardSettings = { height: 600, width: 600, step: 7 };
 const mapObjects = loadMapObjects();
-// const spaces = loadSpace(mapObjects, settings);
 export const RoutePage: React.FC = () => {
   const route = new NavigationMapObject(mapObjects);
-
   const mapObjectsAndRoute = [...mapObjects, route];
   return <Board mapObjects={mapObjectsAndRoute} settings={settings} />;
 };
--- libs/map-routing/src/index.ts
+++ libs/map-routing/src/index.ts
@@ -1,4 +1,5 @@
 export { Board } from './lib/board';
+export { BoardSettings } from './lib/map-items/board-context'
 export * from './lib/map-items/scaffolding';
 export * from './lib/map-items/navigation-path/navigation-map-object';
 export * from './lib/map-items/map-object';
