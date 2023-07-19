29e45afb08cefd7f66675b976111ada91bf5bb98
Audrius <audrysbut@gmail.com>
2023-07-19 07:57:58 +0300

    chore: minor imrovements

--- apps/web-experiments/src/app/NavigationBar.tsx
+++ apps/web-experiments/src/app/NavigationBar.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./NavigationBar.css";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './NavigationBar.css';
 
 export const NavigationBar = () => {
   const navigate = useNavigate();
@@ -8,22 +8,22 @@ export const NavigationBar = () => {
     <table className="table">
       <thead>
         <tr>
-          <th className="th" onClick={() => navigate("/rubik")}>
+          <th className="th" onClick={() => navigate('/rubik')}>
             Rubik
           </th>
-          <th className="th" onClick={() => navigate("/sudoku")}>
+          <th className="th" onClick={() => navigate('/sudoku')}>
             Sudoku solver
           </th>
-          <th className="th" onClick={() => navigate("/clock")}>
+          <th className="th" onClick={() => navigate('/clock')}>
             Clock
           </th>
-          <th className="th" onClick={() => navigate("/puzzle")}>
+          <th className="th" onClick={() => navigate('/puzzle')}>
             Puzzle
           </th>
-          <th className="th" onClick={() => navigate("/graph")}>
+          <th className="th" onClick={() => navigate('/graph')}>
             Graph
           </th>
-          <th className="th" onClick={() => navigate("/route")}>
+          <th className="th" onClick={() => navigate('/route')}>
             Route
           </th>
         </tr>
